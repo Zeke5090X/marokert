49081862c2a3f02d1ad38ca6763287842b8952a6
Zeke5090X <zekeloyer2004@gmail.com>
2019-09-09 00:35:19 -0700

    Initial commit

---
+++ .vscode/cquery_cached_index/c@@users@vexlabuser@desktop@marokert/src@globals.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+pros::controller master(E_CONNTROLLER_MASTER);
+pros::controller slave(E_CONNTROLLER_PARTNER);
+
+pros::motor FL(1);
+pros::motor BL(2);
+pros::motor FR(3);
+pros::motor BR(4);
---
+++ src/globals.cpp
@@ -0,0 +1,26 @@
+#include "main.h"
+pros::Controller master(pros::E_CONNTROLLER_MASTER);
+pros::Controller slave(pros::E_CONNTROLLER_PARTNER);
+
+pros::Motor FL(1);
+pros::Motor BL(2);
+pros::Motor FR(3);
+pros::Motor BR(4);
+//drive functions
+//bool arcade;
+int leftdrive;
+int rightdrive;
+
+void cv(){
+leftdrive = master.get_analog(ANALOG_LEFT_Y);
+rightdrive = master.get_analog(-ANALOG_RIGHT_Y);
+}
+
+void vroomvroom(){
+	//if(arcade==!arcade){
+		FL.move(leftdrive);
+		BL.move(leftdrive);
+		FR.move(rightdrive);
+		BR.move(rightdrive);
+	//}
+}
